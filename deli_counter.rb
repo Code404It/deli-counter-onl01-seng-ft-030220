ee4be52fe1f325e8cec12480c4da2f012e1a8658
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-08 19:31:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
-
-def line(array)
-  if array.length==0
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.length==0
     puts "The line is currently empty."
   else
     current_line= "The line is currenty:"
