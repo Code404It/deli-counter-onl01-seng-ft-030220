06e90afa3c04abf0d49e9b3d56e6316d86c08232
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-08 18:08:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array.length== 0
     puts "The line is currently empty."
   else
-    current_line = "The line is currently:"
+    current_line= "The line is currently:"
     array.each.with_index (1) do |name, index|
       current_line<<"#{index}. #{name}"
     end
