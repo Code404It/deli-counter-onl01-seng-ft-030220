11628da4459fcb19aa4a9ecd30d2cf9d73d56ef5
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-08 16:34:02 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,7 @@
+def take_a_number(katz_deli, names)
+  katz_deli.each.with_index[1] do |index, name|
+    katz_deli.push("#{index}. #{name}")
+  end
+    puts "The line is: #{katz_deli.join(" ")}"
+  end
+end
