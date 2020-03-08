34beee1cb1206e1cc38ae00257a40b1f02016f19
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-08 19:17:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,14 @@
-
+def line(array)
+  if array.length==0
+    puts "The line is currently empty."
+  else
+    current_line= "The line is currenty:"
+    array.each.with_index(1) do |name, index|
+      current_line<< "#{index}. #{name}"
+    end
+    puts current_line
+  end
+end
 
 def take_a_number(katz_deli, name)
   katz_deli<< name
