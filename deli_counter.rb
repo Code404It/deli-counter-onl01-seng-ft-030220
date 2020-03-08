01378e2661f8a517e7d6f16030118cceab4bd892
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-08 19:14:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,4 @@
-def line(array)
-  if array.length== 0
-    puts "The line is currently empty."
-  else
-    current_line= "The line is currently:"
-    array.each.with_index(1) do |name,index|
-      current_line<<"#{index}.#{name}"
-    end
-    puts current_line
-end
-end
+
 
 def take_a_number(katz_deli, name)
   katz_deli<< name
